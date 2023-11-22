dc864901731e4d60df28d5000ee2763d29cd0877
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-22 17:28:13 +0100

    add footer and header and home view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup>
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+
+        }
+    },
+
+    components: {
+        AppHeader,
+        AppFooter,
+    },
+
+}
 
 </script>
 
-<template></template>
+<template>
+    <AppHeader />
+
+    <main>
+        <router-view></router-view>
+    </main>
+
+    <AppFooter />
+</template>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    name: 'HomeView'
+}
+</script>
+<template>
+    <div class="container">
+
+        <h1 class="display-4 py-5">
+            Welcome to our cocktails bar!🍸
+        </h1>
+
+        <div class="row">
+            <div class="col-4">
+                <div class="card border-dark">
+                    <div class="card-header">
+                        title
+                    </div>
+                    <div class="card-body">
+                        image
+                    </div>
+                    <div class="card-footer">
+                        infos
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card border-dark">
+                    <div class="card-header">
+                        title
+                    </div>
+                    <div class="card-body">
+                        image
+                    </div>
+                    <div class="card-footer">
+                        infos
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card border-dark">
+                    <div class="card-header">
+                        title
+                    </div>
+                    <div class="card-body">
+                        image
+                    </div>
+                    <div class="card-footer">
+                        infos
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
