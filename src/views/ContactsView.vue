026b0955573aac2e292efe2038699c7754d47346
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-11-23 16:03:32 +0100

    add

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -9,13 +9,20 @@ export default {
 <template>
     <div>
 
+
+
         <div class="p-5 mb-4 bg-dark text-light">
             <div class="container py-5">
                 <h1 class="display-5 fw-bold">Scopri di più</h1>
                 <p class="col-md-8 fs-4">Vieni a provare le nostre novità</p>
                 <button class="btn btn-primary btn-lg" type="button">Find out more</button>
             </div>
+
         </div>
+
+
+
+
 
         <div class="container">
             <form action="">
@@ -45,24 +52,57 @@ export default {
 
 
             </form>
- 
-            
-            
+
+
+
         </div>
 
-        <div class="container d-flex ">
-            <div>
-                <span>SUBMIT</span>
+        <div class="container">
+            <div class="d-flex justify-content-center py-3">
+                <span>SEND</span>
             </div>
-            <div>
+            <div class="d-flex justify-content-center">
                 <button type="submit" class="btn btn-success">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send"
-                viewBox="0 0 16 16">
-                <path
-                  d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-              </svg>
-            </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="currentColor" class="bi bi-send"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                    </svg>
+                </button>
             </div>
+        </div>
+
+
+        <div class="bg-dark text-light mt-5">
+            <div class=" d-flex">
+                <div class="col text-center">
+
+
+                    <h2 class="py-5">RECAPITI</h2>
+
+
+
+                    <div class="d-flex justify-content-center align-items-center gap-3">
+                        <img width="50" src="https://th.bing.com/th/id/OIP.55DCXbXlKDgEBoZhKxpzLAAAAA?rs=1&pid=ImgDetMain"
+                            alt="">
+                        <img width="50"
+                            src="https://th.bing.com/th/id/R.26d9974a1feec9905a4e0d5e5ddf8db6?rik=ycoXFwG5Udz08A&pid=ImgRaw&r=0"
+                            alt="">
+                    </div>
+
+                    <h2 class="pt-5">SEDE</h2>
+                    <p>Via palazzo Venezia 12</p>
+                    <p>Pescara (PE)</p>
+
+                </div>
+
+                <div class="col">
+                    <img class="img-fluid" src="https://foundthebar.com/gall/12.jpg" alt="">
+                </div>
+
+            </div>
+
+
         </div>
 
     </div>
